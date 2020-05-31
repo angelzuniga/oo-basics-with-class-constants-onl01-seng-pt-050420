8db7c6fc65aa8e9f1af7d44667edf44b2105b200
Angel Zuniga <angelzuniga@outlook.com>
2020-05-31 22:58:57 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,6 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
+  attr_reader :brand :shoe 
   
   BRANDS = []
   
@@ -14,8 +14,8 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
-  def brand(brand)
-    @brand = brand
-    BRANDS << brand 
+  def shoe(shoe)
+    @shoe = shoe
+    BRANDS << shoe 
   end
 end
